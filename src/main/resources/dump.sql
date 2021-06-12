33623da898cb7e81b2ff6d2259b8ff1184582416
zurdo24 <efrain.ake97@gmail.com>
2021-06-12 13:41:57 -0500

    base de datos que subio mal miguel(le dije que iba a dar errores) corregido

--- src/main/resources/dump.sql
+++ src/main/resources/dump.sql
@@ -1,20 +1,5 @@
--- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema marketapp
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema marketapp
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `marketapp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+CREATE SCHEMA IF NOT EXISTS `marketapp` ;
 USE `marketapp` ;
 
 -- -----------------------------------------------------
@@ -26,12 +11,7 @@ CREATE TABLE IF NOT EXISTS `marketapp`.`user` (
   `password` VARCHAR(255) NULL DEFAULT NULL,
   `email` VARCHAR(255) NULL DEFAULT NULL,
   `type` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 51
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+  PRIMARY KEY (`id`));
 
 -- -----------------------------------------------------
 -- Table `marketapp`.`customer`
@@ -44,16 +24,8 @@ CREATE TABLE IF NOT EXISTS `marketapp`.`customer` (
   `sex` VARCHAR(50) NOT NULL,
   `payment_type` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`user_id`),
-  INDEX `fk_user_id` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `user_id`
     FOREIGN KEY (`user_id`)
-    REFERENCES `marketapp`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+    REFERENCES `marketapp`.`user` (`id`));
 
 -- -----------------------------------------------------
 -- Table `marketapp`.`address`
@@ -66,13 +38,8 @@ CREATE TABLE IF NOT EXISTS `marketapp`.`address` (
   `suburb` VARCHAR(255) NOT NULL,
   `city` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_customer_id` (`customer_id` ASC) VISIBLE,
-  CONSTRAINT `customer_id`
-    FOREIGN KEY (`customer_id`)
-    REFERENCES `marketapp`.`customer` (`user_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+	FOREIGN KEY (`customer_id`)
+    REFERENCES `marketapp`.`customer` (`user_id`));
 
 
 -- -----------------------------------------------------
@@ -88,14 +55,8 @@ CREATE TABLE IF NOT EXISTS `marketapp`.`deliveryman` (
   `address` VARCHAR(255) NOT NULL,
   `vehicle` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`user_id`),
-  INDEX `fk_user_id` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `user_id`
     FOREIGN KEY (`user_id`)
-    REFERENCES `marketapp`.`user` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+    REFERENCES `marketapp`.`user` (`id`));
 
 -- -----------------------------------------------------
 -- Table `marketapp`.`salesman`
@@ -109,13 +70,8 @@ CREATE TABLE IF NOT EXISTS `marketapp`.`salesman` (
   `city` VARCHAR(50) NOT NULL,
   `address` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`user_id`),
-  INDEX `fk_user_id` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `id`
     FOREIGN KEY (`user_id`)
-    REFERENCES `marketapp`.`user` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    REFERENCES `marketapp`.`user` (`id`));
 
 
 -- -----------------------------------------------------
@@ -132,13 +88,8 @@ CREATE TABLE IF NOT EXISTS `marketapp`.`commerce` (
   `address` VARCHAR(255) NOT NULL,
   `logo` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_salesman_id` (`salesman_id` ASC) VISIBLE,
-  CONSTRAINT `salesman_id`
     FOREIGN KEY (`salesman_id`)
-    REFERENCES `marketapp`.`salesman` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `marketapp`.`salesman` (`user_id`));
 
 
 -- -----------------------------------------------------
@@ -152,13 +103,10 @@ CREATE TABLE IF NOT EXISTS `marketapp`.`product` (
   `price` DOUBLE NOT NULL,
   `stock` DOUBLE NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_commerce_id` (`commerce_id` ASC) INVISIBLE,
-  CONSTRAINT `commerce_id`
     FOREIGN KEY (`commerce_id`)
     REFERENCES `marketapp`.`commerce` (`salesman_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+   );
+
 
 
 -- -----------------------------------------------------
@@ -174,19 +122,10 @@ CREATE TABLE IF NOT EXISTS `marketapp`.`order` (
   `total` DOUBLE NOT NULL,
   `payment_type` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `deliveryman_id` (`deliveryman_id` ASC) VISIBLE,
-  INDEX `customer_id` (`customer_id` ASC) VISIBLE,
-  CONSTRAINT `deliveryman_id`
     FOREIGN KEY (`deliveryman_id`)
-    REFERENCES `marketapp`.`deliveryman` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `customer_id`
+    REFERENCES `marketapp`.`deliveryman` (`user_id`),
     FOREIGN KEY (`customer_id`)
-    REFERENCES `marketapp`.`customer` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `marketapp`.`customer` (`user_id`));
 
 
 -- -----------------------------------------------------
@@ -199,20 +138,8 @@ CREATE TABLE IF NOT EXISTS `marketapp`.`order_detail` (
   `subtotal` DOUBLE NOT NULL,
   `finished` TINYINT NOT NULL,
   PRIMARY KEY (`order_id`, `product_id`),
-  INDEX `fk_product_id` (`product_id` ASC) VISIBLE,
-  CONSTRAINT `order_id`
     FOREIGN KEY (`order_id`)
-    REFERENCES `marketapp`.`order` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `product_id`
+    REFERENCES `marketapp`.`order` (`id`),
     FOREIGN KEY (`product_id`)
-    REFERENCES `marketapp`.`product` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `marketapp`.`product` (`id`));
 
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
